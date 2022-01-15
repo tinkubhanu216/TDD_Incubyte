d7715cbfc04782e628c7d00dc226c33e3fd23991
tinkubhanu216 <bhanuprasadthandra@gmail.com>
2022-01-15 22:13:40 +0530

    negatives not allowed exception added

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,7 +1,17 @@
 
 public class StringCalculator {
-	int add(String numbers) {
+	int add(String numbers) throws Exception {
 		int sum=0;
+		int index = numbers.indexOf('-');
+		while(index >= 0) {
+			if(index<numbers.length()-1) {
+				char c=numbers.charAt(index+1); 
+				if(Character.isDigit(c)) {
+					throw new Exception("negatives not allowed");					
+				}
+			}
+		   index = numbers.indexOf('-', index+1);
+		}
 		//Replace all non digit to space
 		String digitStr = numbers.replaceAll("[^\\d]", " ");
 		// Remove extra spaces from the beginning
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -7,16 +7,31 @@ public class StringCalculatorTest {
 	@Test
 	public void test() {
 		StringCalculator calObj=new StringCalculator();
-		assertEquals(0,calObj.add(""),0.0);
+		try {
+			assertEquals(0,calObj.add(""),0.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 	}
 	@Test
 	public void test2() {
 		StringCalculator calObj=new StringCalculator();
-		assertEquals(6,calObj.add("1\\n2,3"),0.0);
+		try {
+			assertEquals(6,calObj.add("1\\n2,3-"),0.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 	}
 	@Test
 	public void test3() {
 		StringCalculator calObj=new StringCalculator();
-		assertEquals(3,calObj.add("//;\\n1;2"),0.0);
+		try {
+			assertEquals(3,calObj.add("//;\\n-1;2"),0.0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
 	}
 }
